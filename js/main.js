ad689b5d511e7ce78b07759544da890549a52820
Zezka69 <gordejfedorov32@gmail.com>
2024-11-09 18:25:39 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+document.getElementById('button_1').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_2').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_3').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_4').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_5').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_6').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_7').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_8').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_9').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
