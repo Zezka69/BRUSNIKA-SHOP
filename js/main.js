02a964aa0a419b16d8faee17a03f2005a90666e5
Zezka69 <gordejfedorov32@gmail.com>
2025-01-11 18:05:13 +0500

    11.01.25

--- js/main.js
+++ js/main.js
@@ -25,3 +25,67 @@ document.getElementById('button_8').addEventListener('click', function menu(link
 document.getElementById('button_9').addEventListener('click', function menu(link) {
     window.open('./BRUSNIKA.html', "_self");
 })
+document.getElementById('button_10').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_11').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_12').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_13').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_14').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_15').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_16').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_17').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_18').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_19').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_20').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_21').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_22').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_23').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_24').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_25').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_26').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_27').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+
+document.getElementById('button_28').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_29').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
+document.getElementById('button_30').addEventListener('click', function menu(link) {
+    window.open('./BRUSNIKA.html', "_self");
+})
